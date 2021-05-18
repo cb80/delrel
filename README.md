d2a55aa1937a3d6e977c5cb4c6a575f8b4e4eae2
Chris Burkert <christian.burkert@sap.com>
2021-05-18 20:27:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+![CI/CD](https://github.com/cb80/delrel/workflows/CI/CD/badge.svg)
+
+# Description
+
+This action can delete releases by tag name. It supports GitHub Enterprise.
+
+# Examples
+```
+uses: cb80/delrel@latest
+with:
+  tag: latest
+```
+
+Use a different token:
+```
+uses: cb80/delrel@latest
+with:
+  tag: latest
+  token: ${{ secrets.MY_TOKEN }}
+```
+
+# Inputs
+
+| Option    | Use       | Default                | Description |
+|-----------|-----------|------------------------|-------------|
+| `tag`     | mandatory |                        | The tag which identifies the release. |
+| `token`   | optional  | `secrets.GITHUB_TOKEN` | The token for authentication and authorization. |
+
+# Credits
+
+This action is inspired by the official [typescript action template][tstpl].
+
+[tstpl]: https://github.com/actions/typescript-action
---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const { owner } = github.context.repo;
+const { repo } = github.context.repo;
+
+const run = async function (): Promise<void> {
+  try {
+    const tag: string = core.getInput('tag');
+
+    core.info(`Tag is: ${tag}`);
+    core.info('Setting up Octokit');
+    const token: string = core.getInput('token');
+    const octokit = github.getOctokit(token);
+
+    core.info(`Searching release by tag: ${tag}`);
+    let dat1;
+
+    try {
+      dat1 = await octokit.rest.repos.getReleaseByTag({ owner, repo, tag });
+    } catch (ex: any) {
+      // No release found is a good thing
+      core.debug(ex.message);
+    }
+    core.debug(dat1);
+
+    if (dat1 !== undefined) {
+      core.info('Deleting release');
+      const { data: resp } = await octokit.rest.repos.deleteRelease({
+        owner,
+        repo,
+        release_id: dat1.data.id
+      });
+
+      core.debug(resp);
+    }
+  } catch (ex: any) {
+    core.setFailed(ex.message);
+  }
+};
+
+run();
