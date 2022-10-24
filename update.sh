67b14b56ef8413da61eb7700d26fc00e5ed0763c
Chris Burkert <burkert.chris@gmail.com>
2022-10-24 20:53:37 +0200

    update dependencies

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# run this as follows to update all dependencies:
+# docker run -it -v $(pwd):/c node:16 /c/update.sh
+
+cd /c
+npx --yes npm-check-updates -u
+npm install -g npm@latest
+npm install
+npm run all
